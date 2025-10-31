c59003a1f80e6f973e48585404fe4660fcc836a8
Borromeu Jr <borromeujunior@hotmail.com>
2025-10-31 15:26:10 +0000

    Area para coleta e exibição de dados das 2 cartas

--- Logica Super Trunfo Aventureiro.c
+++ Logica Super Trunfo Aventureiro.c
@@ -47,6 +47,42 @@ printf("PIB do País: %.3f (Trilhões de US$)\n", pib1);
 printf("Número de pontos turísticos: %d\n", pontos1);
 printf("Densidade Populacional: %.2f hab/km2\n", densidade1);
 
+// Área para entrada de dados da carta 2
+printf("\n");
 
+printf("*** Cadastre os dados da Carta 2 ***\n");
+
+printf("\n");
+
+printf("Digite o nome do país: ");
+scanf("%s", nomePais2);
+printf("\n");
+printf("Digite a população do País: ");
+scanf("%lu", &populacao2);
+printf("\n");
+printf("Digite a área total do País: ");
+scanf("%f", &area2);
+printf("\n");
+printf("Digite o PIB do País: ");
+scanf("%f", &pib2);
+printf("\n");
+printf("Digite o numero de pontos turísticos do País: ");
+scanf("%d", &pontos2);
+
+printf("\n");
+
+densidade2 = populacao2 / area2; // Calculo da densidade populacional
+
+// Área destinada a exibição dos dados da carta 2
+printf("\n");
+
+printf("Dados inseridos da carta 2: \n");
+printf("\n");
+printf("Nome do país: %s\n", nomePais2);
+printf("População do País: %.2lu Hab\n", populacao2);
+printf("Área total do País: %.2f km2\n", area2);
+printf("PIB do País: %.3f (Trilhões de US$)\n", pib2);
+printf("Número de pontos turísticos: %d\n", pontos2);
+printf("Densidade Populacional: %.2f hab/km2\n", densidade2);
 
 }
